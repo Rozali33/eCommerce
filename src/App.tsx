a804a96d6129bda88d93a0dff051a6fe1763bd07
Rosali <garifullina.r33@gmail.com>
2024-05-29 22:10:57 +0300

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import { SignIn, Main, EmptyPage, SignUp } from './pages';
+import { SignIn, Main, SignUp } from './pages';
+import Error from './pages/Error/Error';
 
 function App() {
   return (
     <Routes>
       <Route path="/signUp" element={<SignUp />} />
       <Route path="/" element={<Main />} />
-      <Route path="*" element={<EmptyPage />} />
+      <Route path="*" element={<Error />} />
       <Route path="/signIn" element={<SignIn />} />
     </Routes>
   );
