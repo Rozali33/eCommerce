11f84854b53953ee8eaecd46b03cea0fd651deeb
Rosali <garifullina.r33@gmail.com>
2024-05-29 22:08:50 +0300

    feat: update Main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -12,12 +12,14 @@ export const Main = () => {
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 px-20">
             {items?.map((item) => (
               <ItemCard
-                key={item.id}
-                price={item.price}
-                imageSrc={item.image}
-                description={item.description}
-                title={item.title}
-              />
+              key={item.id}
+              price={item.price}
+              imageSrc={item.image}
+              description={item.description}
+              title={item.title}
+              category={item.category}
+              rating={item.rating}
+            />              
             ))}
           </div>
         ) : (
